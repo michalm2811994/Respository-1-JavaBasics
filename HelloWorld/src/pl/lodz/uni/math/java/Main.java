8ccc2e5b264c8fc8998e548365c49b6672a9d629
michalm2811994 <michalm2811994@interia.pl>
2017-03-03 00:27:38 +0100

    HelloWorld v0.0.3

--- HelloWorld/src/pl/lodz/uni/math/java/Main.java
+++ HelloWorld/src/pl/lodz/uni/math/java/Main.java
@@ -6,6 +6,15 @@ public class Main {
 
 		Hello helloObliect = new Hello();
 		helloObliect.sayHello();
+		
+		System.out.println("Test");
+		int intVariable = 10;
+		double doubleVariable = 10.55;
+		float floatVariable = 1.55f;
+		
+		System.out.println("\nintVariable - " + intVariable);
+		System.out.println("doubleVariable - " + doubleVariable);
+		System.out.println("floatVariable - " + floatVariable);
 	}
 
 }
