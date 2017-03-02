16e319580cfd2dd24201e1a8bf916f2d1485c472
michalm2811994 <michalm2811994@interia.pl>
2017-03-02 23:57:48 +0100

    Hello

---
+++ HelloWorld/src/pl/lodz/uni/math/java/Hello.java
@@ -0,0 +1,5 @@
+package pl.lodz.uni.math.java;
+
+public class Hello {
+
+}
---
+++ HelloWorld/src/pl/lodz/uni/math/java/Main.java
@@ -0,0 +1,9 @@
+package pl.lodz.uni.math.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
