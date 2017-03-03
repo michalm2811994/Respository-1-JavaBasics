855faa440c6214510d73008bb4221c69172fb6e5
michalm2811994 <michalm2811994@interia.pl>
2017-03-03 20:54:26 +0100

    ClassEmployee

---
+++ ClassEmployee/src/pl/lodz/uni/math/java/Main.java
@@ -0,0 +1,13 @@
+package pl.lodz.uni.math.java;
+
+public class Main {
+
+		public static void main(String[] args) {
+			 	
+			 		Employee empl = new Employee("Kowalski Marian", "Junior Developer", 23, 3400);
+			 		empl.changeSalary(3600);
+			 		empl.printData();
+
+	}
+
+}
